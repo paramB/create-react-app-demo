df2045fb1bffe34b43873ea6d0babcb79b5cb3c7
ParamB <22649951+paramB@users.noreply.github.com>
2020-10-03 14:51:59 +1300

    modified readme and app.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # create-react-app-demo
+A react application demo with no build configuration. 
+
+## Contributors
+- Param Bhangal (https://github.com/paramB)
+- Peter Russell (https://github.com/peterussell)
--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, It's a simple react application development with no configuration build.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
